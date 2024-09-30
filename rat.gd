af44b8aedc3986db972314b997057fc7d4b2d7a5
Shane Liang <shanelkx@gmail.com>
2024-09-30 10:48:42 +0800

    Fixed collision fusing bug

--- rat.gd
+++ rat.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var speed = 100
+@export var player: Node2D
 @export var health : float = 10
 
 func take_damage(amount):
@@ -9,3 +11,10 @@ func take_damage(amount):
 
 func die():
 	queue_free()  # Remove the enemy from the scene
+
+func _physics_process(delta):
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = direction * speed
+		
+		move_and_slide()
