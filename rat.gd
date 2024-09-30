f5c5b631936d342ee9a8a007f8d4f42e300deb7e
Shane Liang <shanelkx@gmail.com>
2024-09-30 11:42:56 +0800

    Added enemy healthbar

--- rat.gd
+++ rat.gd
@@ -4,10 +4,16 @@ extends CharacterBody2D
 @export var player: Node2D
 @export var health : float = 10
 
+@onready var health_bar = $Sprite2D/HealthBar
+
 func take_damage(amount):
 	health -= amount
+	update_health()
 	if health <= 0:
 		die()
+
+func update_health():
+	health_bar.value = health
 
 func die():
 	queue_free()  # Remove the enemy from the scene
