a4835a4c8b310de6b27578f8db870e149ec18563
Shane Liang <shanelkx@gmail.com>
2024-10-07 10:06:12 +0800

    Non-functioning spawn scene

---
+++ spawn_info.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Spawn_Info
+
+@export var time_start: int = 0
+@export var time_end: int = 0
+@export var enemy_spawn_delay: int = 0
+@export var enemy_no: int = 1
+@export var enemy: Resource
+
+var spawn_delay_counter: int = 0
