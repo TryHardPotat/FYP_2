2cb97715a247c3cf13a5af8af5710b123b98de23
Shane Liang <shanelkx@gmail.com>
2024-10-07 10:30:31 +0800

    Another non-functional spawner

--- spawner.gd
+++ spawner.gd
@@ -1,52 +1,24 @@
 extends Node2D
 
-@export var spawn_table : Array[Spawn_Info] = []
+@export var spawn_scene : PackedScene
+@export var spawn_interval : float = 2.0
+@export var spawn_radius : float = 100.0
 
-@onready var player = get_tree().get_first_node_in_group("Warrior")
+func _ready():
+	print("Spawner Started")
+	var timer = Timer.new()
+	timer.connect("timeout", Callable(self, "_on_spawn_timer,timeout"))
+	timer.set_wait_time(spawn_interval)
+	timer.set_one_shot(false)
+	add_child(timer)
+	timer.start()
 
-var time = 0
-
-func _on_timer_timeout():
-	time += 1
-	var enemy_spawns = spawn_table
-	for i in enemy_spawns:
-		if time >= i.time_start and time <= i.time_end:
-			if i.spawn_delay_counter < i.enemy_spawn_delay:
-				i.spawn_delay_counter += 1
-			else:
-				i.spawn_delay_counter = 0
-				var new_enemy_scene = load(i.enemy.resource_path)
-				var counter = 0
-				while counter < i.enemy_no:
-					var enemy_spawn = new_enemy_scene.instantiate()
-					enemy_spawn.global_position = get_random_position()
-					add_child(enemy_spawn)
-					counter += 1
-
-func get_random_position():
-	var viewportrect = get_viewport_rect().size * randf_range(1.1,1.4)
-	var top_left = Vector2(player.global_position.x - viewportrect.x/2, player.global_position.y - viewportrect.y/2)
-	var top_right = Vector2(player.global_position.x + viewportrect.x/2, player.global_position.y - viewportrect.y/2)
-	var bottom_left = Vector2(player.global_position.x - viewportrect.x/2, player.global_position.y + viewportrect.y/2)
-	var bottom_right = Vector2(player.global_position.x + viewportrect.x/2, player.global_position.y + viewportrect.y/2)
-	var pos_side = ["up", "down", "right", "left"].pick_random()
-	var spawn_pos1 = Vector2.ZERO
-	var spawn_pos2 = Vector2.ZERO
-	
-	match pos_side:
-		"up":
-			spawn_pos1 = top_left
-			spawn_pos2 = top_right
-		"down":
-			spawn_pos1 = bottom_left
-			spawn_pos2 = bottom_right
-		"right":
-			spawn_pos1 = top_right
-			spawn_pos2 = bottom_right
-		"left":
-			spawn_pos1 = top_left
-			spawn_pos2 = bottom_left
-	
-	var x_spawn = randf_range(spawn_pos1.x, spawn_pos2.x)
-	var y_spawn = randf_range(spawn_pos1.y, spawn_pos2.y)
-	return Vector2(x_spawn, y_spawn)
+func _on_spawn_timer_timeout():
+	if spawn_scene:
+		var instance = spawn_scene.instantiate()
+		var spawn_position = Vector2(randf_range(-spawn_radius, spawn_radius), randf_range(-spawn_radius, spawn_radius))
+		
+		instance.position = spawn_position
+		add_child(instance)
+	else:
+		print("No Scene to Spawn")
