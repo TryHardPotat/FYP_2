00640d9488ef68a1984138ab159e28de96fb979b
Shane Liang <shanelkx@gmail.com>
2024-10-28 00:00:40 +0800

    Tested autotiling

--- character_select.gd
+++ character_select.gd
@@ -7,22 +7,24 @@ var spawner_node = spawner_scene.instantiate()
 @export var slingshot : PackedScene
 @export var spellcaster : PackedScene
 
+## Character Select Does Not Work
+
 func _on_select_scout_pressed():
-	spawner_node.target_scene = scout
+	spawner_node.target_scene = scout ##
 	start_game()
 
 func _on_upgrade_scout_pressed():
 	pass # Replace with function body.
 
 func _on_select_slingshot_pressed():
-	spawner_node.target_scene = slingshot
+	spawner_node.target_scene = slingshot ##
 	start_game()
 
 func _on_upgrade_slingshot_pressed():
 	pass # Replace with function body.
 
 func _on_select_spellcaster_pressed():
-	spawner_node.target_scene = spellcaster
+	spawner_node.target_scene = spellcaster ##
 	start_game()
 
 func _on_upgrade_spellcaster_pressed():
