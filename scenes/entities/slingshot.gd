c7dcfa3d8a07994fcdff7cd26060de9a6ecec9f6
Shane Liang <shanelkx@gmail.com>
2024-11-12 14:15:02 +0800

    Added exp to all characters

--- scenes/entities/slingshot.gd
+++ scenes/entities/slingshot.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 @onready var movement_component : Movement_Component = $"Movement Component"
 @onready var attack_component : Ranged_Attack_Component = $"Ranged Attack Component"
 @onready var hurtbox : Area2D = $Hurtbox
+@onready var exp_bar : ProgressBar = $CanvasGroup/ExperienceBar
 
 var team : String = "player"
+var player_exp = Global.player_exp
+var player_level = Global.player_level
 
 func _physics_process(_delta):
 	var input_velocity = movement_component.velocity
@@ -14,3 +17,17 @@ func _physics_process(_delta):
 
 func take_damage(amount):
 	hurtbox.take_damage(amount)
+
+func gain_exp(exp_amount):
+	player_exp += exp_amount
+	exp_bar.value = player_exp
+	if player_exp >= exp_bar.max_value:
+		level_up()
+	print("Experience: ", player_exp)
+
+func level_up():
+	player_level += 1
+	player_exp = 0
+	exp_bar.value = 0
+	exp_bar.max_value *= 1.5
+	print("Level Up: Player Level ", player_level)
