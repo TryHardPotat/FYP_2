9d8ef245fa604a1aec286803343323fd7a4b7e2f
Shane Liang <shanelkx@gmail.com>
2024-11-19 12:35:25 +0800

    Added coin icons to upgrade menu

---
+++ scenes/menus/upgrade_screen.gd
@@ -0,0 +1,38 @@
+extends Control
+
+
+
+func _on_scout_attack_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_scout_health_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_scout_speed_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_slingshot_attack_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_slingshot_health_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_slingshot_speed_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_spellcaster_attack_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_spellcaster_health_upgrade_pressed():
+	pass # Replace with function body.
+
+
+func _on_spellcaster_speed_upgrade_pressed():
+	pass # Replace with function body.
